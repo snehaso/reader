9681cb365c93b41e49b4e209f67bce0cf740a424
Sneha S <snehasomwanshi@gmail.com>
2016-01-27 14:34:07 +0530

    Send words per minute to coloring service
    The text is broken down into chunk of words according to reading speed.
    These chunks are sent to coloring service every minute. This controls the sending speed.

--- app/assets/javascripts/reader.js
+++ app/assets/javascripts/reader.js
@@ -1,16 +1,80 @@
 (function() {
-var init = function() {
-  var readerContainer = $("#reader-container");
-  if(readerContainer.size() > 0) {
-    var pusher = new Pusher('da03829d30f7142f7f34', { encrypted: true });
-    var notificationsChannel = pusher.subscribe('notifications');
+  var words;
+  var interval;
 
-    notificationsChannel.bind('new_notification', function(notification){
-        var message = notification.message;
-        $('.word-container').html(message);
+  var init = function() {
+    var readerContainer = $("#reader-container");
+    if(readerContainer.size() > 0) {
+      var pusher = new Pusher('da03829d30f7142f7f34', { encrypted: true });
+      var notificationsChannel = pusher.subscribe('notifications');
+
+      notificationsChannel.bind('new_notification', function(notification){
+          var message = notification.message;
+          $('.word-container').html(message);
+      });
+    }
+    var text = $('.well-lg').text();
+    words = text.split(/\s+/);
+    $("#start-reading-button").on("click", playText);
+    $("#glyphicon-pause-button").on("click", pauseText);
+    $("#glyphicon-resume-button").on("click", resumeText);
+
+    $(".dropdown-menu li a").click(function(){
+      $(this).parents(".btn-group").find('.selection').text($(this).text());
+      $(this).parents(".btn-group").find('.selection').attr("value", $(this).attr('value'));
     });
-  }
-};
+  };
+
+  var playText = function () {
+    $('#start-reading-button').attr('disabled','disabled');
+    var wordsPerMin = parseInt($('.reading-speed .dropdown-toggle .selection').attr('value'));
+    interval = setInterval(function() {
+      if(words.length > 0) {
+        sendWordToColoringApi(words.shift());
+      }
+      else{
+        clearInterval(interval);
+      }
+
+    }, 60000/parseInt(wordsPerMin));
+
+    $('#start-reading-button').removeAttr('disabled');
+    $('#glyphicon-pause-button').removeAttr('disabled');
+    $('#glyphicon-resume-button').removeAttr('disabled');
+  };
+
+  var pauseText = function () {
+    clearInterval(interval);
+    delete(interval);
+    $("#start-reading-button").attr('disabled','disabled');
+    $('#glyphicon-pause-button').attr('disabled','disabled');
+    $('#glyphicon-resume-button').removeAttr('disabled');
+  };
+
+  var resumeText = function () {
+    playText();
+    $("#start-reading-button").attr('disabled','disabled');
+    $('#glyphicon-pause-button').removeAttr('disabled');
+    $('#glyphicon-resume-button').attr('disabled','disabled');
+  };
+
+  var sendWordToColoringApi = function(word){
+    var colorScheme = $('.color-scheme .dropdown-toggle .selection').attr('value');
+    $.ajax({
+      url: 'http://localhost:4567/color',
+      type: 'POST',
+      contenttype:'application/json; charset=utf-8',
+      data: JSON.stringify({
+        words: [word],
+        color_scheme: colorScheme
+      }),
+      crossDomain: true,
+      dataType:'json',
+      success: function() {
+        alert( "success" );
+      }
+    });
+  };
 
   $(document).ready(function() {
     init();
